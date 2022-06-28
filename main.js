f694407a9a8f57f7728a0a944882891e79eada59
NotJhuz <99780276+IdkJhus@users.noreply.github.com>
2022-06-28 16:05:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ const { state, saveState } = useSingleFileAuthState(global.authFile)
 const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
-browser: ['Jaashi <3','Firefox','1.0.0'],
+browser: ['NotJhuz-MD','Brave','1.0.0'],
 }
 
 global.conn = makeWASocket(connectionOptions)
