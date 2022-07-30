3eae91861150b47787743ddad6bfefcbe6a5c1b3
NotJhuz <99780276+IdkJhus@users.noreply.github.com>
2022-07-30 11:39:38 -0600

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -2,25 +2,31 @@
 export async function before(m, { isAdmin, isBotAdmin }) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Awkawk Orang Asin :l')
+conn.reply('No permitimos esos codes aqui :D :l')
 conn.groupParticipantsUpdate(m.chat, [user], "remove")
    }
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Awkawk Orang Asin :l')
+conn.reply('No permitimos esos codes aqui :D :l')
 conn.groupParticipantsUpdate(m.chat, [user], "remove")
    } 
    
    if (m.sender.startsWith('92' || '92')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Awkawk Orang Asin :l')
+conn.reply('No permitimos esos codes aqui :D :l')
 conn.groupParticipantsUpdate(m.chat, [user], "remove")
    } 
    
    if (m.sender.startsWith('90' || '90')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Awkawk Orang Asin :l')
+conn.reply('No permitimos esos codes aqui :D :l')
+conn.groupParticipantsUpdate(m.chat, [user], "remove")
+   } 
+   
+   if (m.sender.startsWith('1' || '1')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('No permitimos esos codes aqui :D :l')
 conn.groupParticipantsUpdate(m.chat, [user], "remove")
    } 
     }
